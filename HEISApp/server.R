bf256251d4564d41a0d9f78b1329cb8567cfcd1c
Arin Shahbazian <arin1989@gmail.com>
2020-01-09 17:00:38 +0330

    Add new dataset (in progress) + some changes in code

--- HEISApp/server.R
+++ HEISApp/server.R
@@ -11,12 +11,12 @@ app_server <- function(input, output,session) {
         gs <- input$slcT2Grp
         st <- input$slcT2Stat
 
-        fn <- paste0("data/Y",substr(y,3,4),"Merged4CBN.rda")
+        fn <- paste0("data/Y",substr(y,3,4),"MergedData.rda")
         load(fn)
 
         fnc <- get(st)
 
-        MD[,lapply(.SD,fnc), by=gs,.SDcols=vs]
+        MergedData[,lapply(.SD,fnc), by=gs,.SDcols=vs]
     })
 
     output$pltT2barchart <- renderPlot({
@@ -26,12 +26,12 @@ app_server <- function(input, output,session) {
         gs <- input$slcT2Grp
         st <- input$slcT2Stat
 
-        fn <- paste0("data/Y",substr(y,3,4),"Merged4CBN.rda")
+        fn <- paste0("data/Y",substr(y,3,4),"MergedData.rda")
         load(fn)
 
         fnc <- get(st)
 
-        SD <- MD[,lapply(.SD,fnc), by=gs,.SDcols=vs]
+        SD <- MergedData[,lapply(.SD,fnc), by=gs,.SDcols=vs]
 
         if(length(gs)>1){
             SD[,NewVar:=do.call(paste0,.SD),.SDcols=gs]
